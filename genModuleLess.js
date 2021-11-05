4e9ee6f14c42449ab374256109d7f84785745c74
kaipeng.yao <kaipeng.yao@e-research-solutions.com>
2021-11-05 11:05:37 +0800

    🐞 fix: 調整less文件過濾目錄

--- genModuleLess.js
+++ genModuleLess.js
@@ -19,7 +19,7 @@ const genModuleLess = (parents, { isModule, filterFileLess }) => {
   lessArray = [];
   glob
     .sync(winPath(`${parents}/**/**.less`), {
-      ignore: ['**/node_modules/**', '**/dist/**', '**/es/**', '**/lib/**', '**/_site/**'],
+      ignore: ['**/node_modules/**', '**/dist/**', '**/es/**', '**/_site/**'],
     })
     .sort((a, b) => lessOrder(a) - lessOrder(b))
     .filter(filePath => {
